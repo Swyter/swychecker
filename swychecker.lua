3ef8f89b1363b9b0173283c00f70af777d9056c6
Swyter <Swyter@Portatil>
2012-09-11 14:38:46 +0200

    *Now checks for sounds. Same codebase.

--- swychecker.lua
+++ swychecker.lua
@@ -1,7 +1,7 @@
 local x = os.clock()
 swydec = {}
 
-local filename = {"R:\\Juegos\\swconquest\\modules\\swconquest-branch\\scenes.txt"}
+local filename = {"R:\\Juegos\\swconquest\\modules\\swconquest-branch\\sounds.txt"}
 
 local s,raw=0,{}
 for l in io.lines(filename[1]) do
@@ -13,14 +13,15 @@ end
 local count=tonumber(raw[2][1])
 local sum=0
 local scns={}
-for i=3,count*4,4 do
+for i=3,count do
   scns[#scns+1]=raw[i][1]
-  if not io.open(filename[1]:match("(.+)scenes.txt").."sceneobj\\"..raw[i][1]..".sco") then
+  if  io.open(filename[1]:match("(.+)sounds.txt").."sounds\\"..raw[i][1]) and
+      io.open(filename[1]:match("(.+)sounds.txt").."..\\..\\sounds\\"..raw[i][1])  then
     print(raw[i][1])
     sum=sum+1
   end
 end
- print(sum.." missing SCOs")
+ print(sum.." missing OGGs")
  
 
  
@@ -28,10 +29,10 @@ end
 -- Lua implementation of PHP scandir function
 function scandir(directory)
     local i, t, popen = 0, {}, io.popen
-    for filename in popen('dir "'..filename[1]:match("(.+)scenes.txt")..'sceneobj\\*.sco" /b'):lines() do
+    for filename in popen('dir "'..filename[1]:match("(.+)sounds.txt")..'sounds\\*.ogg" /b'):lines() do
         --print(filename)
         i = i + 1
-        t[i] = filename:match("(.+).sco")
+        t[i] = filename
     end
     return t
 end
@@ -54,6 +55,6 @@ for _,a in pairs(test) do
     found=false
 end
 
- print("these "..sum.." unused SCOs are in folder but not referenced in scenes.txt")
+ print("these "..sum.." unused OGGs are in folder but not referenced in sounds.txt")
 
  print(os.clock()-x.."s")
